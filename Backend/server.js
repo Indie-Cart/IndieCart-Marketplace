9780b7a362f0823362e102979970c5d9beda59d2
Muhammed Safi <2552075@students.wits.ac.za>
2025-04-10 23:42:34 +0200

    connected backend and frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-const PORT = 8080;
+//test api
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'blue',
+        size: 'large'
+    })
+})
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
+app.get('/*splat', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+app.listen(8080, () => {
+    console.log(`It's alive on http://localhost:8080`)
 });
