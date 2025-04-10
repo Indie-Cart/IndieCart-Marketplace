cb9e08475a03e20664065e3be07ec37c63a90576
umigadat <2655559@students.wits.ac.za>
2025-04-10 14:23:34 +0200

    setup react in the frontend folder using vite and added login/autho

---
+++ Frontend/src/HomePage.jsx
@@ -0,0 +1,18 @@
+function Header(){
+  return(
+      <header>
+          <meta charset="UTF-8"></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+          <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
+          <h1 class="shop-name">🌸 Indie Cart 🌸</h1>
+          <nav>
+              <ul>
+                  <li><a href="#" >Home</a></li>
+                  <li><a href="#">Become a Seller</a></li>
+              </ul>
+          </nav>
+      </header>
+  )
+}
+
+export default Header
