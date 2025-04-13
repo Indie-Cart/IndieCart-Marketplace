61ca17168ba9438a9c37dde5aa99820265abd554
Muhammed Safi <2552075@students.wits.ac.za>
2025-04-13 17:21:08 +0200

    setup testing for both backend and frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   server: {
     port: 3000,  // Set port to 3000
     open: true,  // Optionally, open the browser automatically
